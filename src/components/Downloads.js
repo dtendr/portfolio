eadf736ebf097feed3ee9b1ea052d9d6b3227f8c
Josh Davis <dtendr@gmail.com>
2018-01-24 10:27:10 -0500

    Updated downloads, added project files

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-/*import cakexna from "../assets/projects/cake-raid-xna.zip";*/
+import { withPrefix } from 'gatsby-link';
 
 const Downloads = () => (
   <section id="downloads">
@@ -16,34 +15,20 @@ const Downloads = () => (
           <div className="twelve columns">
 
             <p>
-              Cake Raid XNA
-              { /*<a href={cakexna} title="Cake Raid (XNA)">Download</a>*/}
+              <a href={ withPrefix('/projects/cake-raid-xna.zip') } title="Cake Raid (XNA)">Cake Raid (XNA)</a>
             </p>
             <p>
-              Hole Punch
-              { /*<a href={cakexna} title="Cake Raid (XNA)">Download</a>*/}
+              <a href={ withPrefix('/projects/hole-punch.zip') } title="Hole Punch">Hole Punch</a>
             </p>
             <p>
-              Tile Dungeon, prototype build
-              { /*<a href={cakexna} title="Cake Raid (XNA)">Download</a>*/}
+              <a href={ withPrefix('/projects/TileDungeon.swf') } title="Tile Dungeon">Tile Dungeon, prototype build</a>
             </p>
             <p>
-              Renu, demo build
-              { /*<a href={cakexna} title="Cake Raid (XNA)">Download</a>*/}
+              <a href="http://adamettenberger.com/ImagineCup2013/play/" title="Renu">Renu, demo build</a>
             </p>
             <p>
-              The Rock (v1.4, for use with Nuclear Dawn)
-              { /*<a href={cakexna} title="Cake Raid (XNA)">Download</a>*/}
+              <a href={ withPrefix('/projects/The_Rock_(1.4).zip') } title="The Rock">The Rock (v1.4, for use with Nuclear Dawn)</a>
             </p>
-
-
-            {/* old links, need updating
-
-            <li><a href="projects/cake-raid-xna.zip" title="Cake Raid (XNA)">Cake Raid (XNA)</a></li>
-            <li><a href="projects/hole-punch.zip" title="Hole Punch">Hole Punch</a></li>
-            <li><a href="projects/TileDungeon.swf" title="Tile Dungeon">Tile Dungeon, prototype build</a></li>
-            <li><a href="http://adamettenberger.com/ImagineCup2013/play/" title="Renu">Renu, demo build</a></li>
-            <li><a href="projects/The_Rock_(1.4).zip" title="The Rock">The Rock (1.4)</a></li>*/}
             
           </div>
         </div>
